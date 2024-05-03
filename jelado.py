267a6ccccf87ec9b4f89f383c790d05dc5167f1a
R0lk0 <116081448+R0lk0@users.noreply.github.com>
2024-05-03 13:51:34 +0200

    masodik komit

--- jelado.py
+++ jelado.py
@@ -6,11 +6,11 @@ class Jelek:
         self.x = x
         self.y = y
 
-def eltelt():
-    mbpe = sor[0]*360 + sor[1]*60 + sor[2]
+def mbpe():
+    sor[0]*360 + sor[1]*60 + sor[2]
 
 lista = []
-f = open('jel.txt', 'rt', encoding='utf-8')
+f = open('jelado/jel.txt', 'rt', encoding='utf-8')
 for sor in f:
     sor = sor.strip().split(' ')
     lista.append(Jelek(sor[0], sor[1], sor[2], sor[3], sor[4]))
